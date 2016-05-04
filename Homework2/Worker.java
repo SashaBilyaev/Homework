45b5e5e0f103fcc2ca60cb280356b2a774746901
SashaBilyaev <Sanuk0988@gmail.com>
2016-05-04 08:42:05 +0300

    Update Worker.java

--- Homework2/Worker.java
+++ Homework2/Worker.java
@@ -13,25 +13,37 @@ public class Worker {
         this.surname = surname;
     }
 
+    public String getSurname() {return surname;}
+
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getName() {return name;}
 
     public void setWorkshop(String workshop) {
         this.workshop = workshop;
     }
 
+    public String getWorkshop() {return workshop;}
+
     public void setCategory(int category) {
         this.category = category;
     }
+
+    public int getCategory() {return category;}
 
     public void setRoomCabinet (int roomCabinet) {
         this.roomCabinet = roomCabinet;
     }
 
+    public int getRoomCabinet() {return roomCabinet;}
+
     public void setHomeAddress(String homeAddress) {
         this.homeAddress = homeAddress;
     }
+
+    public String getHomeAddress() {return homeAddress;}
 
     public String toString() {
         return   "Surname: " + this.surname + "\nName: " + this.name + "\nWorkshop: " + this.workshop + "\nCategory: " + this.category + "\nRoomCabinet: " + this.roomCabinet + "\nHomeAddress: " + this.homeAddress;
